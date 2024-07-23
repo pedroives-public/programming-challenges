45b73a1f309195ae907d6632ecb1a2800c8dec65
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-07-22 22:10:33 -0300

    Add Sort the People problem from LeetCode in Python

---
+++ solutions/sort_the_people.py
@@ -0,0 +1,36 @@
+"""
+You are given an array of strings names, and an array heights 
+that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+
+Example 1:
+
+    Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+    Output: ["Mary","Emma","John"]
+    Explanation: Mary is the tallest, followed by Emma and John.
+
+Example 2:
+
+    Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+    Output: ["Bob","Alice","Bob"]
+    Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+
+Problem Source: LeetCode
+
+Solution -> O(n*log(n))
+"""
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        tup = []
+        for i in range(len(names)):
+            tup.append((names[i], heights[i]))
+        
+        tup = sorted(tup, key=lambda x: x[1], reverse=True)
+        people = [person[0] for person in tup]
+        
+        return people
