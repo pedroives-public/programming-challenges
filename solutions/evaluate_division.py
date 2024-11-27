dd3a742d17547457524f26a01a12a460b3b40aea
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2024-11-26 23:44:16 -0300

    Add Evaluate Division problem from LeetCode in Python

---
+++ solutions/evaluate_division.py
@@ -0,0 +1,68 @@
+"""
+You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+Note: The variables that do not occur in the list of equations are undefined, so the answer cannot be determined for them.
+
+Example 1:
+
+    Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+    Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+    Explanation: 
+    Given: a / b = 2.0, b / c = 3.0
+    queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? 
+    return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+    note: x is undefined => -1.0
+
+Example 2:
+
+    Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+    Output: [3.75000,0.40000,5.00000,0.20000]
+
+Example 3:
+
+    Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+    Output: [0.50000,2.00000,-1.00000,-1.00000]
+
+
+Problem Source: LeetCode
+
+Solution -> O(E + Q)
+"""
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        for i in range(len(equations)):
+            u, v = equations[i][0], equations[i][1]
+            graph[u].append((v, values[i]))
+            graph[v].append((u, 1/values[i]))
+
+        def dfs(node, end, visited, prod):
+            if node not in graph:
+                return -1.0
+                
+            if node == end:
+                return prod
+
+            visited.add(node)
+            for path, value in graph[node]:
+                if path not in visited:
+                    result = dfs(path, end, visited, prod * value) 
+                    if result != -1:
+                        return result
+
+            return -1.0
+
+        result = []
+        for start, end in queries:
+            visited = set()
+            check = dfs(start, end, visited, 1.0)
+            result.append(check)
+            
+        return result
