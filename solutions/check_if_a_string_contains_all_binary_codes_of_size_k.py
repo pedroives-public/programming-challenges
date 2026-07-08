c6fc0a0a93b16218fcfdebd08a9d152c94c335f2
Pedro Ives <47249629+pedroives-public@users.noreply.github.com>
2026-07-07 22:18:31 -0300

    Add Check If a String Contains All Binary Codes of Size K problem from LeetCode in Python

---
+++ solutions/check_if_a_string_contains_all_binary_codes_of_size_k.py
@@ -0,0 +1,39 @@
+"""
+Given a binary string s and an integer k, return true if every binary code of length k is a substring of s. Otherwise, return false.
+
+Example 1:
+
+  Input: s = "00110110", k = 2
+  Output: true
+  Explanation: The binary codes of length 2 are "00", "01", "10" and "11". They can be all found as substrings at indices 0, 1, 3 and 2 respectively.
+
+Example 2:
+
+  Input: s = "0110", k = 1
+  Output: true
+  Explanation: The binary codes of length 1 are "0" and "1", it is clear that both exist as a substring. 
+
+Example 3:
+
+  Input: s = "0110", k = 2
+  Output: false
+  Explanation: The binary code "00" is of length 2 and does not exist in the array.
+
+
+Problem Source: LeetCode
+
+Solution -> O(n)
+"""
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        unique = {}
+        for i in range(len(s) - k + 1):
+            code = s[i:i+k]
+            if code not in unique:
+                unique[code] = 1
+        
+        if len(unique) < 2 ** k:
+            return False
+        
+        return True
